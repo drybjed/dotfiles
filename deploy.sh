afc5e739c3615ea65cde4e97ca0a84f61d05f001
Maciej Delmanowski <drybjed@gmail.com>
2013-05-20 10:43:37 +0200

    Added simple deployment script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# dotfiles deployment script
+
+repository="git://github.com/drybjed/dotfiles"
+repodir="$HOME/.Vault/github.com/drybjed"
+dotdir="$HOME/.Vault/github.com/drybjed/dotfiles"
+
+mkdir -p $repodir
+cd $repodir
+git clone $repository
+
+ln -s $dotdir/.zsh $HOME/.zsh
+ln -s $dotdir/.zlogin $HOME/.zlogin
+ln -s $dotdir/.zshenv $HOME/.zshenv
+ln -s $dotdir/.zshrc $HOME/.zshrc
+ln -s $dotdir/.vimrc $HOME/.vimrc
+ln -s $dotdir/.tmux.conf $HOME/.tmux.conf
+
