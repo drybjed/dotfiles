df9e9598d6eedc542559e6f4645ddd9b1fb677b6
Maciej Delmanowski <drybjed@gmail.com>
2015-01-21 11:34:36 +0100

    Turn off screen after 30 seconds when locked

--- .config/i3/i3lock.sh
+++ .config/i3/i3lock.sh
@@ -7,5 +7,13 @@ if $(type scrot > /dev/null && type convert > /dev/null) ; then
   scrot "${tmp}/locked.png" -e 'convert $f -blur 6x6 $f.blur ; rm -f $f'
 fi
 
+# Activate screen saver, turn off screen after 30 seconds of inactivity
+xset s on
+xset s 30
+xset s blank
+
 i3lock --nofork --color=#111111 --image=${tmp}/locked.png.blur
+
+# Disable screen saver
+xset s off
 
